fb87da071ad4efc71d76551391f8b97a5cae2a50
martinnc <martinncook@gmail.com>
2025-09-17 14:51:42 +0100

    All basic pages plus advice scripts

---
+++ src/content.config.ts
@@ -0,0 +1,19 @@
+// Import the glob loader
+import { glob } from "astro/loaders";
+// Import utilities from `astro:content`
+import { z, defineCollection } from "astro:content";
+// Define a `loader` and `schema` for each collection
+const advice = defineCollection({
+  loader: glob({ pattern: "**/[^_]*.md", base: "./src/advice" }),
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    order: z.number(),
+    image: z.object({
+      url: z.string(),
+      alt: z.string(),
+    }),
+  }),
+});
+// Export a single `collections` object to register your collection(s)
+export const collections = { advice };
